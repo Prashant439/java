a5fff582d42fed52e3bc27b3bf81c7910ef7c710
unknown <prashantankalagi.xworkz@gmail.com>
2024-06-24 12:41:17 +0530

    this is my first commit

---
+++ Rules.java
@@ -0,0 +1,7 @@
+public class Rules{
+	//static variables
+	public static int rulesForWomen=0;
+	public static int rulesForMen=100;
+	public static String rulz="Rules are needed for better environment";
+	
+}
