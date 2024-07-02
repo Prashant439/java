a57b50e2080ede943ebc4fad85606320dccabe87
unknown <prashantankalagi.xworkz@gmail.com>
2024-07-02 21:10:34 +0530

    2/7/2024

---
+++ HouseRunner.java
@@ -0,0 +1,5 @@
+class HouseRunner{
+ public static void main(String args[]){
+ House.HouseDetails("vidya nagar Gokak.", "Shankar", "Malati", "Prashant");
+ }
+}
---
+++ PaintRunner.java
@@ -0,0 +1,6 @@
+class PaintRunner{
+ public static void main(String args[]){
+ 
+ Paint.displayPaintDetails("Red", "Glossy", "Acrylic", 5000);
+ }
+}
